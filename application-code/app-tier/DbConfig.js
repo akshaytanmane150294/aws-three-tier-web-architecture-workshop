3eb3c89fac53dbc21a0c1ea5262dbc598b5d792a
akshaytanmane150294 <akshaytanmane1@gmail.com>
2024-08-13 17:22:18 -0400

    added files

---
+++ application-code/app-tier/DbConfig.js
@@ -0,0 +1,6 @@
+module.exports = Object.freeze({
+    DB_HOST: 'database-threetier-webapp-instance-1.c5kay8g0u99c.us-east-1.rds.amazonaws.com',
+    DB_USER: 'admin',
+    DB_PWD: 'admin12345',
+    DB_DATABASE: 'webappdb'
+});
